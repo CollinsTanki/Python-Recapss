efa5923740199a90f8c1e2b8a7e03b736df36b96
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2026-02-01 07:43:46 +0300

    logical not operator use case

---
+++ logical_not.py
@@ -0,0 +1,7 @@
+# If applicant has high_income and has no criminal reco
+
+has_high_income = True
+has_criminal_record = False
+
+if  has_high_income and not has_criminal_record:
+    print("Elligible for loan")

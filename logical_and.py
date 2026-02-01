39bd4763b2498c7372e347b8d84cce29a285eb6e
Collins Tanki <51945008+CollinsTanki@users.noreply.github.com>
2026-02-01 07:16:50 +0300

    logical and use case

---
+++ logical_and.py
@@ -0,0 +1,6 @@
+#iF  Applicant has high income AND good credit then he/she is elligible for loan
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("Elligible for loan")
